47f0a62ad99b38c1001e1fab8ed90e74abd84bce
Diana Toth <dianatoth.dtt@gmail.com>
2021-03-14 12:02:53 +0200

    updated big cup with the percentage and remaining water

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
-
+updateBigCup()
 smallCups.forEach((cup, index) => {
     console.log(index)
     cup.addEventListener('click', () => highlightCups(index))
@@ -22,4 +22,27 @@ function highlightCups(index) {
             cup.classList.remove('full')
         }
     })
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    console.log(fullCups)
+    const totalCups = smallCups.length
+    console.log(totalCups)
+    if (fullCups === 0) {
+        percentage.style.visibility = "hidden"
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups/totalCups*330}px`
+        percentage.innerText = `${fullCups/totalCups*100}%`
+    }
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2-(250*fullCups/1000)} L`
+    }
 }
